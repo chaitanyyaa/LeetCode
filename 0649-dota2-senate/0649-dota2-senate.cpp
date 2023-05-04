4922f50ba0821096a61f137bf59558d8f65fbf4c
Chaitanya Nanadikar <105532837+chaitanyyaa@users.noreply.github.com>
2023-05-04 11:04:03 +0530

    Time: 4 ms (93.51%), Space: 7.3 MB (68.32%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int count = 0, len = 0;
+        while (senate.size() != len) {
+            string s;
+            len = senate.size();
+            for (int i = 0; i < len; i++) {
+                if (senate[i] == 'R') {
+                    if (count++ >= 0) s += 'R';
+                }
+                else if (senate[i] == 'D') {
+                    if (count-- <= 0) s += 'D';
+                }
+            }  
+            swap(s, senate);
+        }
+        if (senate[0] == 'R') 
+            return "Radiant";
+        else 
+            return "Dire";               //RDDRDDRD   r =3 d = 5   s = RDDR   
+    }
+};
