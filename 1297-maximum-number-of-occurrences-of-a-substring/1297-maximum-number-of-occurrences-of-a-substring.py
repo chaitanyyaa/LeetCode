3b5793a0dfd24d0a346f28339d09fd3b57045514
Chaitanya Nanadikar <105532837+chaitanyyaa@users.noreply.github.com>
2023-05-21 14:44:41 +0530

    Time: 168 ms (58.55%), Space: 18.6 MB (44.36%) - LeetHub

---
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
+        # count frequency of characters
+        count = collections.Counter()
+        
+        # Check only for minSize
+        for i in range(len(s) - minSize + 1):
+            t = s[i : i+minSize]
+            if len(set(t)) <= maxLetters:
+                count[t] += 1
+        
+        return max(count.values()) if count else 0
